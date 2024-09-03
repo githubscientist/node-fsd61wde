99ae4ea4be363e406c4d9773b353a49855049693
githubscientist <sathish.datascientist@gmail.com>
2024-09-03 12:28:23 +0530

    updated cors

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // use the express middleware to parse JSON bodies
